2e647a47233ebdf55cdebbc87b9664cb4baaf880
Ty4jbt <tyaboe@gmail.com>
2021-10-09 00:08:06 -0700

    updated login function

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { FetchDataApiService } from '../fetch-api-data.service';
 
@@ -11,23 +12,27 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./user-login-form.component.scss']
 })
 export class UserLoginFormComponent implements OnInit {
-
-  @Input() userData = { username: '', password: ''}
-
+  
+  @Input() userDetails = { username: '', password: ''}
+  
   constructor(
     public fetchApiData: FetchDataApiService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
-    public snackBar: MatSnackBar
-  ) { }
-
-  ngOnInit(): void { }
-
-  loginUser(): void {
-    this.fetchApiData.userLogin(this.userData).subscribe(() => {
-      this.dialogRef.close();
-      this.snackBar.open('Login successful', 'OK', {
-        duration: 2000
-      });
+    public snackBar: MatSnackBar,
+    public router: Router
+    ) { }
+    
+    ngOnInit(): void { }
+    
+    loginUser(): void {
+      this.fetchApiData.userLogin(this.userDetails).subscribe((response) => {
+        localStorage.setItem('username', response.user.username);
+        localStorage.setItem('token', response.token);
+        this.dialogRef.close();
+        this.snackBar.open('Login successful', 'OK', {
+          duration: 2000
+        });
+        this.router.navigate(['movies']);
     }, (response) => {
       this.snackBar.open(response, 'OK', {
         duration: 2000
